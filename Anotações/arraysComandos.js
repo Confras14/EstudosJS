e2aeaec428e0935118d1fa1de122693a140eff19
Lucas Conrado <lucas.c.souza14115@gmail.com>
2023-12-31 00:41:38 -0300

    Arrumando e explicando melhor os comandos de arrays

--- Anotações/arraysComandos.js
+++ Anotações/arraysComandos.js
@@ -1,34 +1,45 @@
-let nomes = [
-  'Lucas',
-  'Julio',
-  'Ana',
-  'Alex',
-  'Roberta',
-  'Julia',
-  'Lucas'
-]
-console.log(nomes)
+let cores = ['Azul', 'Verde', 'Vermelho', 'Amarelo', 'Ciano', 'Rosa']
 
-nomes.push('Paulo')
-console.log(nomes)
+// Lembrando que a contagem de termos para os localizar se incia no 0 e não no 1
+// ex: o termo 1 da array acima não é o azul e sim o verde.
 
-nomes = nomes.slice(1,6)
-console.log(nomes)
+console.log(cores)
 
-nomes.shift()
-console.log(nomes)
+// length mostra a quantidade de termos na array
+console.log(`Tamanho do Array ${cores.length}`)
 
-nomes.pop()
-console.log(nomes)
+// Remove o primeiro termo da array
+cores.shift()
+console.log(cores)
 
-nomes.splice(1, 1)
-console.log(nomes)
+// Remove o ultimo termo da array
+cores.pop()
+console.log(cores)
 
-nomes.reverse()
-console.log(nomes)
+// Adiciona um termo no final da array
+cores.push("Preto")
+console.log(cores)
 
-let arrayNome = nomes[0].split('')
-console.log(arrayNome)
+// Junta todos os elementos da array e tranforma em uma string
+// Dentro das aspas é o que vai ser escrito entre os termos
+console.log(cores.join(", "))
 
-let allNames = nomes.join(' ')
-console.log(allNames)
+// Para alterar um valor da array apenas escolha o termo com base na sua pocição
+cores[1] = 'Cinza'
+console.log(cores)
+
+// Para alterar o ultimo valor da array
+cores[cores.length - 1] = 'Branco'
+console.log(cores)
+
+// Para Selecionar os termos que estiverem entre o primeiro e ultimo valor passado
+console.log(cores.slice(1,3))
+
+// Usado para a partir do termo escolhido (primeiro valor) imprir a quantidade escolhida de termos (segundo valor)
+console.log(cores.splice(1, 2))
+
+// Inverte a ordem dos termos da array
+console.log(cores.reverse())
+
+// Se para uma string char por char e a tranforma em um array
+console.log(cores[1].split(''))
